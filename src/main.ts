02877b2ea19c53fceb12e6478a65b398a3af9f47
Duyanhdda <anh.haduyanh@hcmut.edu.vn>
2021-12-26 18:16:26 +0700

    make it better

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,8 @@ import * as session from "express-session";
 import flash = require("connect-flash");
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.useStaticAssets(join(__dirname, '..', 'public'));
   app.enableCors();
+  app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('pug');
   app.use(cookieParser());
